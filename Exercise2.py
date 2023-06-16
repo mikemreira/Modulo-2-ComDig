cdc7f721c5430ad2ab4182c7a157f205619d0f90
Miguel Moreira <miguelmoreira2009@hotmail.com>
2023-06-16 22:01:54 +0100

    Progresso

--- Exercise2.py
+++ Exercise2.py
@@ -1,14 +1,20 @@
 import serial
+import array
+import time
+import FletcherChecksumLib as fletcher
 
-arduino = serial.Serial(port='/dev/cu.usbserial-10', baudrate=9600)
-
-
-def main():
-    while True:
-        if arduino.in_waiting:
-            packet = arduino.readlines()
-            print(packet)
+arduino = serial.Serial(port='/dev/cu.usbserial-10', baudrate=9600, timeout=3)
 
 
-if __name__ == '__main__':
-    main()
+def geometric_progression(n, u, r):
+    arr = array.array("f", (0 for _ in range(0, n)))
+    for i in range(0, n):
+        arr[i] = (u * r) ** i
+    return arr
+
+
+while 1:
+    time.sleep(3)
+    receivedStr = arduino.readline()  # .rstrip().decode("utf-8")
+    print(fletcher.FletcherChecksumBytes.get_fletcher32(receivedStr))
+
--- arduino_script/arduino_script.ino
+++ arduino_script/arduino_script.ino
@@ -1,13 +1,12 @@
-char input;
- 
+#include "Fletcher.h"
+
 void setup() {
-    Serial.begin(9600); 
-    delay(2000);  
+    Serial.begin(9600);   
 }
- 
+
 void loop() {
-    if(Serial.available()){
-        input = Serial.read();
-        Serial.println(input);
-    }
+  uint16_t text[] = {1, 3, 9, 27, 81, 243, 729};
+  uint32_t fl = fletcher32(text, 7, 0, 0);
+  Serial.println(fl);
+  delay(10000);
 }
